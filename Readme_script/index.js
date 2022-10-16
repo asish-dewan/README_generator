5ac44354c743cff7235749172be63f8c88aa2440
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-10-16 22:28:40 +1100

    Added validation function to relevant questions

--- Readme_script/index.js
+++ Readme_script/index.js
@@ -1,22 +1,91 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const generateHTML = ({ name, location, github, linkedin }) =>
+//const generateREADME = ({ username, location, github, linkedin }) =>
 
+// Inquirer questions
 
 inquirer
     .prompt([
     {
         type: "input",
-        name: "name",
-        message: "What is your name?",
+        name: "username",
+        message: "What is your GitHub username?",
+        // Validate if the user has provided a value
+        validate: (value) => {
+            if(value) {return true}
+                else {return 'Please enter a valid input'}
+        },
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+        // Validate if the user has provided a value
+        validate: (value) => {
+            if(value) {return true}
+                else {return 'Please enter a valid input'}
+        },
+    },
+    {
+        type: "input",
+        name: "project",
+        message: "What is your project title?",
+        // Validate if the user has provided a value
+        validate: (value) => {
+            if(value) {return true}
+                else {return 'Please enter a valid input'}
+        },
+    },
+    {
+        type: "input",
+        name: "project-description",
+        message: "Please write a brief description of your project",
+        // Validate if the user has provided a value
+        validate: (value) => {
+            if(value) {return true}
+                else {return 'Please enter a valid input'}
+        },
     },
     
-    ])
-    .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+    {
+        type: "list",
+        name: "license",
+        message: "Please select a license type",
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None' ],
+    },
+    {
+        type: "input",
+        name: "dependencies",
+        message: "What command should be run to install dependencies?",
+        default: 
 
-    fs.writeFile("index.html", htmlPageContent, (err) =>
-        err ? console.log(err) : console.log("Successfully created index.html!")
-    );
-    });
+    },
+
+    {
+        type: "input",
+        name: "dependencies",
+        message: "What command should be run to run tests?"
+    },
+
+    {
+        type: "input",
+        name: "dependencies",
+        message: "What does the user need to know about using the repo?"
+        // Validate if the user has provided a value
+        validate: (value) => {
+            if(value) {return true}
+                else {return 'Please enter a valid input'}
+        },
+    },
+    {
+        type: "input",
+        name: "dependencies",
+        message: "What does the user need to know about contributing to the repo?"
+        // Validate if the user has provided a value
+        validate: (value) => {
+            if(value) {return true}
+                else {return 'Please enter a valid input'}
+        },
+    }
+    ])
