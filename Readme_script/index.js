282987b3ea8397dd7d6d512675bc655286b48154
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-10-16 23:38:17 +1100

    Added Write file function + initialise app function

--- Readme_script/index.js
+++ Readme_script/index.js
@@ -1,13 +1,11 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require ('./util');
 
-const generateREADME = require ('./util/Markdown');
+const MarkdownGenerator = require ('./util/Markdown');
 
 // Inquirer questions
 
-inquirer
-    .prompt([
+const questions = [
     {
         type: "input",
         name: "username",
@@ -55,24 +53,31 @@ inquirer
         message: "Please select a license type",
         choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None' ],
     },
+
     {
         type: "input",
         name: "dependencies",
         message: "What command should be run to install dependencies?",
-        default: 
-
+        validate: (value) => {
+            if(value) {return true}
+                else {return 'Please enter a valid input'}
+    },
     },
 
     {
         type: "input",
         name: "tests",
-        message: "What command should be run to run tests?"
+        message: "What command should be run to run tests?",
+        validate: (value) => {
+            if(value) {return true}
+                else {return 'Please enter a valid input'}
+    },
     },
 
     {
         type: "input",
         name: "usage",
-        message: "What does the user need to know about using the repo?"
+        message: "What does the user need to know about using the repo?",
         // Validate if the user has provided a value
         validate: (value) => {
             if(value) {return true}
@@ -82,11 +87,40 @@ inquirer
     {
         type: "input",
         name: "contribution",
-        message: "What does the user need to know about contributing to the repo?"
+        message: "What does the user need to know about contributing to the repo?",
         // Validate if the user has provided a value
         validate: (value) => {
             if(value) {return true}
                 else {return 'Please enter a valid input'}
         },
     }
+    ];
+
+    // Function to write README file
+
+    const writeToFile = (fileName, data) => {
+
+        fs.writeFile(fileName, data, function (err) {
+            if (err) {
+                return console.log (err)
+            }
+            else {
+                console.log ("Success!")
+            }
+        })
+    }
+
+
+    // Initialise application
+
+    function init () {
+        inquirer.prompt(questions)
+        .then(function(data) {
+            writeToFile ("README.md", MarkdownGenerator(data));
+        })
+    }
+
+    // Call init function
+
+    init();
 
